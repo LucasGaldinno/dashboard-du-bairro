3878f3c8a8dfdbb3df06b81bdfeeef4eb533ca17
Lucas Galdino <lucaas.galdinno@outlook.com>
2022-02-02 19:01:38 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1>Dashboard do restaurante Du-Bairro  	:chart_with_upwards_trend:</h1>
+
+Atividade desenvolvida em grupo, esse projeto foi uma continuação do projeto Du-Bairro restaurante, é um dashboard desenvolvido para calcular os gasto da empresa Du-Bairro. Nele você consegue está visualizando o quanto a empresa lucra e você consegue ver o total de vendas da empresa, a quantidade de clientes que visitam a empresa  etc.
+
+<h2>Integrantes do grupo:pushpin:</h2>
+
+- <h3><a href="https://github.com/LucasAlvesM">Lucas Alves</a></h3>
+- <h3><a href="https://github.com/LucasGaldinno">Lucas Galdino</a></h3>
+- <h3><a href="https://github.com/ryandcmv">Ryan Marinho</a></h3> 
+- <h3><a href="https://github.com/Tiagogtr">Tiago Soares</a></h3>
+- <h3><a href="https://github.com/VitorGeovani">Vitor Geovani</a></h3>
+
+<h2>Funções de cada integrante do grupo:clipboard:</h2>
+
+ - Lucas Alves ----> Criar o menu lateral :heavy_check_mark:
+ - Lucas Galdino ----> Criar os gráficos :heavy_check_mark:
+ - Ryan Marinho ----> Responsividade do dashboard :heavy_check_mark:
+ - Tiago Soares ----> Responsável em estilizar e posicionar os elementos do Header :heavy_check_mark:
+ - Vitor Geovani ----> Criar os gráficos :heavy_check_mark:
+
+<h2>Ferramenta utilizada para o desenvolvimento do projeto:toolbox:</h2>
+Esse projeto foi desenvolvido com o visual studio code versão: February 2021 (version 1.54)<br>
+<a href="https://code.visualstudio.com/">Download</a>
+
+<h2>Conclusão do projeto:dart:</h2>
+
+<img src="https://raw.githubusercontent.com/LucasGaldinno/dashboard-du-bairro/main/Screenshots/dash.png">
---
+++ js/dashboard.js
@@ -0,0 +1,245 @@
+/* globals Chart:false, feather:false */
+
+
+var ctx = document.getElementById('myChart1').getContext('2d');
+var myChart1 = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+        datasets: [{
+            label: 'Vendas Mensais',
+            data: [70000, 75000, 53000, 35000, 42000, 83000, 90000, 120005, 130000, 140000, 160000, 250000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var ctx = document.getElementById('myChart2').getContext('2d');
+var myChart2 = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Picanha', 'Frango a Parmegiana', 'Costela BBQ', 'Arroz Carreteiro', 'Cebola Assada', 'Galeto Assado'],
+        datasets: [{
+            label: 'Pratos mais Vendidos',
+            data: [120000,102000 , 30000, 50000, 20000, 30000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var ctx = document.getElementById('myChart3').getContext('2d');
+var myChart3 = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+        datasets: [{
+            label: 'Lucro Mensal',
+            data: [80, 70, 90, 50, 100, 160,200,300,400,500,450,700],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+var ctx = document.getElementById('myChart4').getContext('2d');
+var myChart4 = new Chart(ctx, {
+    type: 'polarArea',
+    data: {
+        labels: ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado', 'Domingo'],
+        datasets: [{
+            label: '# of Votes',
+            data: [340, 220, 230, 325,307, 210, 160],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+var ctx = document.getElementById('myChart5').getContext('2d');
+var myChart5 = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['1 SEMESTRE' , '2 SEMESTRE'],
+        datasets: [{
+            label: 'Pratos mais Vendidos',
+            data: [600000, 800000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+var canvas = document.getElementById("myChart6");
+var ctx = canvas.getContext('2d');
+
+// Global Options:
+ Chart.defaults.global.defaultFontColor = 'black';
+ Chart.defaults.global.defaultFontSize = 16;
+
+var data = {
+    labels: ["Pessoas que não gostaram", "Pessoas que gostaram"],
+      datasets: [
+        {
+            fill: true,
+            backgroundColor: [
+                'black',
+                'white'],
+            data: [5, 95],
+// Notice the borderColor 
+            borderColor:	['black', 'black'],
+            borderWidth: [3,3]
+        }
+    ]
+};
+
+// Notice the rotation from the documentation.
+
+var options = {
+        title: {
+                  display: true,
+                  text: 'Satisfação dos Clientes - 2020',
+                  position: 'top'
+              },
+        rotation: -0.7 * Math.PI
+};
+
+
+// Chart declaration:
+var myBarChart = new Chart(ctx, {
+    type: 'pie',
+    data: data,
+    options: options
+});
